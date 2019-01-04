768b39bd945a513155ee3af46ea20b0105ac02aa
袁常鑫 <729056559@qq.com>
2019-01-04 14:40:36 +0800

    update

--- groovy/Template.groovy
+++ groovy/Template.groovy
@@ -1,12 +1,10 @@
 package somnous.y.templateservice
 
-import lombok.Data
 
 /**
  *
  * @version 1.0 created by yuanchangxin_fh on 2019/1/4 9:43
  */
-@Data
 class Template {
 
     HashMap map = new HashMap()
