e7eb7c3d854ddde3ddab153e73ffdfc2ecd4f782
yuanchangxin <32184630+yuanchangxin@users.noreply.github.com>
2019-01-04 16:18:24 +0800

    Update Template.groovy

--- groovy/Template.groovy
+++ groovy/Template.groovy
@@ -1,18 +1,2 @@
-package somnous.y.templateservice
-
-
-/**
- *
- * @version 1.0 created by yuanchangxin_fh on 2019/1/4 9:43
- */
-class Template {
-
-    HashMap map = new HashMap()
-
-    def getData = {
-        m->
-            map.putAll(m)
-            return map
-    }
-
-}
+def query = testService.testQuery(2L)
+return query
